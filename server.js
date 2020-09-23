32be7466e0e12d76530d54c415e083ab159b1cbc
Mikhail <mishafrolov882000@gmail.com>
2020-09-23 19:31:34 -0400

    Added Personal Header

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************************  
+ * WEB422 – Assignment 1
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Mikhail Frolov  Student ID: 164788184    Date: Sep. 23, 2020
+ * Heroku Link: _______________________________________________________________
+ *
+ ***********************************************************************************************/
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
